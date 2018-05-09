09dbbd6adef4c13a38a85927ef2efa5f0c3311cc
Yang, Hao <hao-yang@uiowa.edu>
2018-05-08 21:09:06 -0500

    add describe

--- server/server.test.js
+++ server/server.test.js
@@ -3,27 +3,35 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return page not found response', (done) => {
-  request(app)
-    .get('/')
-    .expect(404)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        error: 'Page not found.'
-      });
-    })
-    .end(done);
-});
+describe('Server', () => {
+  
+  describe('GET /', () => {
+    it('should return page not found response', (done) => {
+      request(app)
+        .get('/')
+        .expect(404)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            error: 'Page not found.'
+          });
+        })
+        .end(done);
+    });
+  });
 
-it('should return my user object', (done) => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        name: 'Hao',
-        age: 24
-      });
-    })
-    .end(done);
+  describe('GET /users', () => {
+    it('should return my user object', (done) => {
+      request(app)
+        .get('/users')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            name: 'Hao',
+            age: 24
+          });
+        })
+        .end(done);
+    });
+  });
+
 });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,53 +1,60 @@
 const expect = require('expect');
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-  var res = utils.add(33, 11);
+describe('Utils', () => {
   
-  expect(res).toBe(44).toBeA('number');
-}); 
+  describe('#add', () => {
+    it('should add two numbers', () => {
+      var res = utils.add(33, 11);
 
-it('should async add two numbers', (done) => {
-  utils.asyncAdd(4, 3 , (sum) => {
-    expect(sum).toBe(7).toBeA('number');
-    done();
+      expect(res).toBe(44).toBeA('number');
+    });
+    
+    it('should async add two numbers', (done) => {
+      utils.asyncAdd(4, 3 , (sum) => {
+        expect(sum).toBe(7).toBeA('number');
+        done();
+      });
+    });
   });
-});
 
-it('should square a number', () => {
-  var res = utils.square(5);
-  
-  expect(res).toBe(25).toBeA('number');
-});
+  it('should square a number', () => {
+    var res = utils.square(5);
 
-it('should async square a number', (done) => {
-  utils.asyncSquare(5, (res) => {
     expect(res).toBe(25).toBeA('number');
-    done();
   });
+
+  it('should async square a number', (done) => {
+    utils.asyncSquare(5, (res) => {
+      expect(res).toBe(25).toBeA('number');
+      done();
+    });
+  });
+
+  it('should set firstName and lastName', () => {
+    var user = {location: 'Iowa city', age: 24};
+    var res = utils.setName(user, 'Hao Yang');
+
+    expect(res).toInclude({
+      firstName: 'Hao',
+      lastName: 'Yang'
+    });
+  });
+
+  it('should expect some values', () => {
+    //expect(12).toNotBe(11);
+    //expect({name: 'Hao'}).toEqual({name: 'Hao'});
+    //expect([2,3,4]).toInclude(3);
+    //expect([2,3,4]).toExclude(5);
+    expect({
+      name: 'Hao',
+      age: 24,
+      location: 'Iowa City'
+    }).toInclude({
+      age: 24
+    });
+  });
+
 });
 
-it('should set firstName and lastName', () => {
-  var user = {location: 'Iowa city', age: 24};
-  var res = utils.setName(user, 'Hao Yang');
-  
-  expect(res).toInclude({
-    firstName: 'Hao',
-    lastName: 'Yang'
-  });
-});
-
-it('should expect some values', () => {
-  //expect(12).toNotBe(11);
-  //expect({name: 'Hao'}).toEqual({name: 'Hao'});
-  //expect([2,3,4]).toInclude(3);
-  //expect([2,3,4]).toExclude(5);
-  expect({
-    name: 'Hao',
-    age: 24,
-    location: 'Iowa City'
-  }).toInclude({
-    age: 24
-  });
-});
 
